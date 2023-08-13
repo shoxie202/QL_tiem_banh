6aec59bd9312f268186129b3e0c64cef2f2489ab
hangocdiep <133090487+hangocdiep@users.noreply.github.com>
2023-08-13 14:00:06 +0700

    Add files via upload

---
+++ Views/about.php
@@ -0,0 +1,158 @@
+<!--[if lt IE 8]>
+	<p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+<![endif]-->
+<!-- pages-title-start -->
+<div class="pages-title section-padding">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <div class="pages-title-text text-center">
+                    <h2>Giới Thiệu</h2>
+                    <ul class="text-left">
+                        <li><a href="?act=home">Trang chủ</a></li>
+                        <li><span> // </span>Giới Thiệu</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- pages-title-end -->
+<!-- about-us-section-start -->
+<div class="pages about-us section-padding">
+    <div class="container">
+        <div class="row">
+            <div class="main-padding section-padding-top clearfix">
+                <div class="col-sm-12 col-md-5">
+                    <div class="about-img">
+                        <img src="public/img/products/ab1.png" alt="" />
+                    </div>
+                </div>
+                <div class="col-sm-12 col-md-6">
+                    <div class="about-text">
+                        <div class="about-author">
+                            <h4>Tiệm ăn cô Trà</h4>
+                        </div>
+                        <h5>Thư ngỏ</h5>
+                        <!-- <ol>
+							<li>Chế độ trả góp ưu đãi</li>
+							<li>Thái độ phục vụ tốt</li>
+							<li>Nguồn cung cấp uy tín và ổn định</li>
+							<li>Chế độ bảo hành hợp lý</li>
+						</ol> -->
+                        <p>Xuất phát từ mong muốn đem những đặc sản dân dã miền Tây ngon – sạch – chuẩn tới những người
+                            yêu ẩm thực Việt,
+                            Tiệm bánh cô Trà đã khởi đầu với chỉ một cửa hàng nhỏ và một vài loại bánh. Trải qua nhiều
+                            năm không ngừng nỗ lực
+                            và cố gắng, Tiệm bánh cô Trà hiện đã trở thành cửa hàng có mặt tại vị trí đắc địa trong
+                            thành phố
+                            Thái Nguyên.</p>
+
+                        <p>Với đa dạng các loại mặt hàng như bánh tét, bánh pía Khmer, bánh in và đặc biệt là cá khô hảo
+                            hạng,
+                            Tiệm bánh cô Trà tự hào đem ẩm thực miền Tây chính gốc tới thực khách cả trong và ngoài
+                            nước.Là một người con của miền Tây,
+                            mang trong mình cả 2 dòng máu Việt và Khmer, Trà Ngọc Hằng – người sáng lập Tiệm bánh cô Trà
+                            –
+                            đặt nhiều kì vọng vào thương hiệu hơn là lợi nhuận. Nữ diễn viên,ca sĩ, người mẫu nổi tiếng
+                            một thời chia sẻ:
+                            “Khi bắt tay vào kinh doanh, chắc chắn điều đầu tiên phải chú trọng sẽ là lợi nhuận. Nhưng
+                            với tôi, lợi nhuận không chỉ là tiền bạc,
+                            mà còn là sự hài lòng của khách hàng, uy tín cho thương hiệu và đặc biệt, niềm tự hào về quê
+                            hương nguồn cội.
+                            Tôi muốn mỗi sản phẩm tại Tiệm bánh cô Trà sẽ mang theo không chỉ chất lượng, hương vị, mà
+                            còn cả bản sắc văn hoá và niềm
+                            tự hào của mảnh đất miền Tây”Quá trình phát triển và những sản phẩm chủ lực của thương hiệu
+                            Cô Trà</p>
+
+                        <p>Được thành lập từ năm 2018, Tiệm bánh cô Trà được biết tới rộng rãi thông qua các sản phẩm
+                            chủ lực: Bánh pía Khmer,
+                            bánh tét, bánh in, các loại cá khô, lạp xưởng đặc sản của miền Tây. Khác với những thương
+                            hiệu kinh doanh cùng lĩnh vực,
+                            sản phẩm của Tiệm bánh cô Trà được đầu tư mạnh về phần thiết kế, bao bì. Các mặt hàng như cá
+                            khô, lạp xưởng được xử lý,
+                            đóng hút chân không sạch sẽ và tiện lợi, phù hợp để biếu, tặng, mang đi xa.Hiện tại,Cô Trà
+                            đang có 1 cửa hàng duy nhất tại TP Thái Nguyên.
+                            Hệ thống hotline, fan page và nhân viên tư vấn online chuyên nghiệp, phục vụ 24/7 giúp đáp
+                            ứng mọi yêu cầu từ thực khách, thuận tiện để khách hàng mua sắm.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- about-us section end -->
+<!-- team-member section start -->
+<section class="pages team-member section-padding-bottom">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <div class="section-title text-center">
+                    <!-- <h3>Đội ngũ hỗ trợ</h3> -->
+                </div>
+            </div>
+        </div>
+        <div class="row text-center">
+            <!-- <div class="col-sm-12 col-md-4">
+				<div class="single-member">
+					<div class="member-img">
+						<a href="#"><img src="public/img/about/m1.jpg" alt="Team Member" /></a>
+					</div>
+					<div class="share-member">
+						<div class="member-title">
+							<h4>Dương Xuân Dưỡng</h4>
+						</div>
+						<div class="member-btn">	
+							<a href="#"><i class="mdi mdi-facebook"></i></a>
+							<a href="#"><i class="mdi mdi-twitter"></i></a>
+							<a href="#"><i class="mdi mdi-google-plus"></i></a>
+							<a href="#"><i class="mdi mdi-dribbble"></i></a>
+							<a href="#"><i class="mdi mdi-rss"></i></a>
+						</div>
+					</div>
+				</div>
+			</div> -->
+            <!-- single member end -->
+            <!-- <div class="col-sm-12 col-md-4">
+				<div class="single-member">
+					<div class="member-img">
+						<a href="#"><img src="public/img/about/m2.jpg" alt="Team Member" /></a>
+					</div>
+					<div class="share-member">
+						<div class="member-title">
+							<h4>Trịnh Quang Phúc</h4>
+						</div>
+						<div class="member-btn">	
+							<a href="#"><i class="mdi mdi-facebook"></i></a>
+							<a href="#"><i class="mdi mdi-twitter"></i></a>
+							<a href="#"><i class="mdi mdi-google-plus"></i></a>
+							<a href="#"><i class="mdi mdi-dribbble"></i></a>
+							<a href="#"><i class="mdi mdi-rss"></i></a>
+						</div>
+					</div>
+				</div>
+			</div> -->
+            <!-- single member end -->
+            <!-- <div class="col-sm-12 col-md-4">
+				<div class="single-member">
+					<div class="member-img">
+						<a href="#"><img src="public/img/about/m3.jpg" alt="Team Member" /></a>
+					</div>
+					<div class="share-member">
+						<div class="member-title">
+							<h4>Nguyễn Hữu Tuấn</h4>
+						</div>
+						<div class="member-btn">	
+							<a href="#"><i class="mdi mdi-facebook"></i></a>
+							<a href="#"><i class="mdi mdi-twitter"></i></a>
+							<a href="#"><i class="mdi mdi-google-plus"></i></a>
+							<a href="#"><i class="mdi mdi-dribbble"></i></a>
+							<a href="#"><i class="mdi mdi-rss"></i></a>
+						</div>
+					</div>
+				</div>
+			</div> -->
+        </div>
+    </div>
+</section>
+<!-- team-member section end -->
