a125809b93590ac9a3c94de616984e58314f1780
Nguyễn Xuân Thao <121486519+shoxie202@users.noreply.github.com>
2023-08-14 09:33:42 +0700

    Add files via upload

---
+++ Views/shop/category.php
@@ -0,0 +1,73 @@
+<div class="sidebar left-sidebar">
+    <div class="s-side-text">
+        <div class="sidebar-title clearfix">
+            <h4 class="floatleft">Danh mục</h4>
+        </div>
+        <div class="categories left-right-p">
+            <ul id="accordion" class="panel-group clearfix">
+                <?php    $i = 1; foreach ($data_chitietDM as $row) { ?>
+                <li class="panel">
+                    <div data-toggle="collapse" data-parent="#accordion" data-target="#collapse<?=$i?>">
+                        <div class="medium-a">
+                            <b><?= $data_danhmuc[$i-1]['CategoryName'] ?></b>
+                        </div>
+                    </div>
+                    <div class="paypal-dsc panel-collapse collapse" id="collapse<?=$i?>">
+                        <div class="normal-a">
+                            <?php foreach ($row as $value) { ?>
+                            <a
+                                href="?act=shop&sp=<?= $value['CategoryID'] ?>&loai=<?= $value['TypeproductName'] ?>"><?= $value['TypeproductName'] ?></a>
+                            <?php } ?>
+                        </div>
+                    </div>
+                </li>
+                <?php $i++;
+             } ?>
+            </ul>
+        </div>
+    </div>
+    <div class="s-side-text">
+        <div class="sidebar-title">
+            <h4>Giá</h4>
+        </div>
+        <div class="range-slider clearfix">
+            <form action="index.php?act=shop" method="post">
+                <label><span>Giá: </span> <input type="text" id="amount" readonly /></label>
+                <input type="hidden" id="amount2" name="shop" />
+                <div id="slider-range"></div></br>
+                <button type="submit">Tìm kiếm</button>
+            </form>
+        </div>
+    </div>
+    <div class="s-side-text">
+        <div class="sidebar-title clearfix">
+            <h5 class="floatleft">Các loại khác</h5>
+        </div>
+        <div class="brands-select clearfix">
+            <ul>
+                <li>
+                    <?php for($i=0;$i<4;$i++) {?>
+                    <a
+                        href="?act=shop&sp=1&loai=<?=$data_loaisp[$i]['TypeproductName']?>"><?=$data_loaisp[$i]['TypeproductName']?></a>
+                    <?php } ?>
+                </li>
+                <li>
+                    <?php for($i=4;$i<8;$i++) { ?>
+                    <a
+                        href="?act=shop&sp=1&loai=<?=$data_loaisp[$i]['TypeproductName']?>"><?=$data_loaisp[$i]['TypeproductName']?></a>
+                    <?php  }?>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <div class="s-side-text">
+        <div class="banner clearfix">
+            <a href="?act=detail&id=<?=$data_noibat['ProductID']?>"><img
+                    src="./public/<?=$data_noibat['ProductImage1']?>" alt="" /></a>
+            <div class="banner-text">
+                <h2>Sản phẩm</h2> <br />
+                <h2 class="banner-brand">Quan tâm</h2>
+            </div>
+        </div>
+    </div>
+</div>
